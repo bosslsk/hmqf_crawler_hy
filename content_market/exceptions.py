f98e2cc4789f3960d3dc158d8641e93a2180a7c0
heyao <heyao@haimaqingfan.com>
2017-12-14 14:43:07 +0800

    定义异常

---
+++ content_market/exceptions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+create on 2017-12-14 上午11:18
+
+author @heyao
+"""
+
+
+class BaseException(Exception):
+    pass
+
+
+class HostNotSupportException(BaseException):
+    pass
