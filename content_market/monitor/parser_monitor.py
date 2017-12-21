327c93087bbf1389f7e7d3873b136a5cb28e49f2
heyao <heyao@haimaqingfan.com>
2017-12-21 15:49:03 +0800

    url_checker修改

--- content_market/monitor/parser_monitor.py
+++ content_market/monitor/parser_monitor.py
@@ -8,7 +8,7 @@ import os
 from copy import deepcopy
 import datetime
 
-from content_market.checker import check_chapter_url, chapter_config
+from content_market.checker import hosts_config, chapter_config, UrlChecker
 from content_market.checker.key_inject import inject
 from content_market.monitor.warm import WarmMonitor
 from content_market.parser import parse
@@ -23,6 +23,7 @@ class BookDetailMonitor(WarmMonitor):
         kwargs.pop('log_name', None)
         kwargs['log_name'] = log_name
         super(BookDetailMonitor, self).__init__(queue, scheduler, *args, **kwargs)
+        self.url_checker = UrlChecker(hosts_config, chapter_config)
 
     def one_step(self, data):
         html = data['html']
@@ -34,8 +35,16 @@ class BookDetailMonitor(WarmMonitor):
         parse_func = config['parse_func']
 
         detail_info = parse(html, url, parser, parse_func)
+        if not detail_info:
+            mongo['book_index'].update_one(
+                {'_id': '%s_%s' % (extra_info['source'], extra_info['book_id'])},
+                {'$set': {'status': 0}}
+            )
+            self.logger.debug('no info for url: %s' % url)
+            return
         detail_info.update(extra_info)
         detail_info['added_at'] = datetime.datetime.now()
+        detail_info['finished_status'] = 1
         mongo['book_detail'].update_one(
             {'_id': '%s_%s' % (detail_info['source'], detail_info['book_id'])},
             {'$set': detail_info},
@@ -44,12 +53,13 @@ class BookDetailMonitor(WarmMonitor):
         detail_info.pop('added_at')
         self.logger.debug('successful parse detail for source %s, book %s\n %s'
                           % (detail_info['source'], detail_info['book_id'], detail_info))
-        chapter_request_data, config = check_chapter_url(url, chapter_config)
+        chapter_request_data, config = self.url_checker.check_chapter_url(url)
         if chapter_request_data:
             chapter_request_data['url_type'] = 'chapter'
             chapter_request_data['config'] = config
             chapter_request_data['extra'] = extra_info
             self.scheduler.schedul(chapter_request_data)
+        return True
 
 
 class BookChapterMonitor(WarmMonitor):
@@ -73,24 +83,28 @@ class BookChapterMonitor(WarmMonitor):
             chapter['source'] = extra_info['source']
             chapter['book_id'] = extra_info['book_id']
             chapter['added_at'] = datetime.datetime.now()
+            chapter['finished_status'] = 0
             result = mongo['book_chapter'].update_one(
                 {'_id': '%s_%s_%s' % (chapter['source'], chapter['book_id'], chapter['chapter_ordinal'])},
-                {'$set': chapter},
+                {'$setOnInsert': chapter},
                 upsert=True
             )
-            print result
-            self.logger.debug('successful parse chapter for source %s, book %s, chapter %s\n %s'
-                              % (extra_info['source'], extra_info['book_id'], chapter['chapter_ordinal'], chapter))
-            chapter.pop('added_at')
-            chapter_request_data = data['config']
-            chapter_request_data = {k: inject(chapter_request_data[k], **chapter) for k in chapter_request_data}
-            if chapter_request_data:
-                chapter_request_data['url_type'] = 'content'
-                chapter_request_data['config'] = config['content_config']
-                chapter_request_data['url'] = chapter_request_data['data_url']
-                extra_info.update(chapter)
-                chapter_request_data['extra'] = extra_info
-                self.scheduler.schedul(chapter_request_data)
+            if not result.matched_count:
+                self.logger.debug('successful parse chapter for source %s, book %s, chapter %s\n %s'
+                                  % (extra_info['source'], extra_info['book_id'], chapter['chapter_ordinal'], chapter))
+                chapter.pop('added_at')
+                chapter_request_data = data['config']
+                chapter_request_data = {k: inject(chapter_request_data[k], **chapter) for k in chapter_request_data}
+                if chapter_request_data:
+                    chapter_request_data['url_type'] = 'content'
+                    chapter_request_data['config'] = config['content_config']
+                    chapter_request_data['url'] = chapter_request_data['data_url']
+                    extra_info.update(chapter)
+                    chapter_request_data['extra'] = extra_info
+                    self.scheduler.schedul(chapter_request_data)
+            else:
+                self.logger.debug('already have book: source %s, book_id %s, chapter_id %s' %
+                                  (extra_info['source'], extra_info['book_id'], chapter['chapter_ordinal']))
 
 
 class BookContentMonitor(WarmMonitor):
@@ -114,6 +128,11 @@ class BookContentMonitor(WarmMonitor):
             os.path.join(global_config.get("FILE_PATH"), str(extra_info['source']), str(extra_info['book_id'])),
             '%s.txt' % extra_info['chapter_ordinal'],
             content
+        )
+        mongo['book_chapter'].update_one(
+            {'_id': '%s_%s_%s' % (extra_info['source'], extra_info['book_id'], extra_info['chapter_ordinal'])},
+            {'$set': {'finished_status': 1}},
+            upsert=True
         )
         self.logger.debug('successful parse content for source %s, book %s, chapter %s\n %s'
                           % (extra_info['source'], extra_info['book_id'], extra_info['chapter_ordinal'], content))
